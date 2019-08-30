49eee295262fc30a3dd24b04bcb7bf91b91a721f
meganjeffcoat <jeffcoat.megan@gmail.com>
2019-08-30 11:11:43 -0500

    set up google-analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,12 @@ module.exports = {
         icon: config.manifestIcon, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      option: {
+        trackingId: 208721925,
+      },
+    },
     'gatsby-plugin-sass',
     'gatsby-plugin-offline',
   ],
